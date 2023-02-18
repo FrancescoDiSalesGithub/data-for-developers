23b75d801b8e0fa8dd8aa17c72700c112c8071b4
Francesco Di Sales <f.disales@hotmail.com>
2023-02-18 15:17:46 +0100

    updating solutions.sql with new values

--- solutions.sql
+++ solutions.sql
@@ -1,3 +1,126 @@
-insert into solutions values('DOCKER','DOCKER1','create a local repository','sudo run docker pull registry; run docker -p 5000:5000; docker tag image:version localhost:5000/image:version; docker push localhost:5000/image:version');
-insert into solutions values('PSQLDB','PSQLDB1','change location to store data','go into psql and type show data directory. with an editor edit the following line on the file /etc/postgresql/9.5/main/postgresql.conf; find the following line: data_directory = /custom_path; restart postgresql. To check the new directory log with psql on your database and run show data_directory');
+INSERT INTO solutions (code,code_problem,problem,solution) VALUES
+	 ('DOCKER','DOCKER1','create a local repository','sudo run docker pull registry; run docker -p 5000:5000; docker tag image:version localhost:5000/image:version; docker push localhost:5000/image:version'),
+	 ('PSQLDB','PSQLDB1','change location to store data','change location to store data,go into psql and type show data directory. with an editor edit the following line on the file /etc/postgresql/9.5/main/postgresql.conf; find the following line: data_directory = /custom_path; restart postgresql. To check the new directory log with psql on your database and run show data_directory'),
+	 ('BASH','BASH1','how to do an if in bash','if [ condition ]; then fi'),
+	 ('NGINX','NGINX1','example of serving static file for nginx','Sure, here there is an example of serving static files with Nginx:
 
+Suppose you have a directory named /var/www/html on your server that contains your static HTML, CSS, and JavaScript files, and you want to serve them using Nginx.
+
+Here there is the Nginx configuration that you could use to serve these static files:
+
+server {
+    listen 80;
+    server_name example.com;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+
+In this configuration:
+
+    The listen directive specifies the IP address and port number that Nginx should listen on for incoming requests.
+    The server_name directive specifies the domain name that Nginx should use to respond to requests.
+    The root directive specifies the root directory where Nginx should look for static files.
+    The index directive specifies the default filename that Nginx should use when a directory is requested (in this case, index.html).
+    The location block specifies how Nginx should handle requests that match the given URL pattern (in this case, /).
+
+In the location block, the try_files directive specifies that Nginx should first try to serve the requested file, and if it does not exist, it should return a 404 error.'),
+	 ('NGINX','NGINX2','common configurations of nginx','Nginx is a popular web server and reverse proxy server that is widely used in web development and hosting. Here are some common configurations of Nginx:
+
+    Serving static files: Nginx can be configured to serve static files such as HTML, CSS, and JavaScript files. This is often done using the root and index directives in the server block of the Nginx configuration file.
+
+    Proxying to application servers: Nginx can also be used as a reverse proxy server to forward requests to application servers such as Node.js, PHP, or Ruby on Rails. This is often done using the proxy_pass directive in the location block of the Nginx configuration file.
+
+    Load balancing: Nginx can be used as a load balancer to distribute incoming requests across multiple application servers. This is often done using the upstream directive and the server directive within the upstream block in the Nginx configuration file.
+
+    SSL termination: Nginx can be configured to terminate SSL (Secure Sockets Layer) connections from clients and pass on unencrypted traffic to the application servers. This is often done using the ssl_certificate and ssl_certificate_key directives in the server block of the Nginx configuration file.
+
+    URL rewriting: Nginx can be configured to rewrite incoming URLs to different URLs or to pass specific URL parameters to application servers. This is often done using the rewrite directive in the location block of the Nginx configuration file.
+
+These are just a few of the many common configurations of Nginx. Nginx is a very flexible web server that can be configured in many different ways to suit the needs of different web applications.'),
+	 ('NGINX','NGINX3','proxy_pass in nginx','proxy_pass is a directive in Nginx that is used to set the URL of an upstream server to which Nginx should forward client requests. This allows Nginx to act as a reverse proxy server, forwarding requests to an application server and then returning the response to the client.
+
+Here is an example of how to use proxy_pass in an Nginx configuration file: server {
+    listen 80;
+    server_name example.com;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+    }
+}
+
+In this example, the listen and server_name directives define the IP address and domain name of the server.
+
+The location block specifies the URL pattern that should be forwarded to the upstream server. In this case, all requests to the server s root URL (/) will be forwarded to the upstream server at http://localhost:3000.
+
+The proxy_set_header directives set the values of the Host and X-Real-IP headers in the HTTP request that is forwarded to the upstream server. These headers can be used by the upstream server to provide a response that is customized to the original client request.
+
+When a client makes a request to the server at example.com, Nginx will forward the request to the upstream server at http://localhost:3000, and then return the response to the client.
+
+proxy_pass is a powerful directive that can be used to implement load balancing, caching, and other advanced features in Nginx. It allows Nginx to act as a middleman between the client and the upstream server, providing an additional layer of flexibility and control over the flow of HTTP requests and responses.'),
+	 ('NGINX','NGINX4','load balancing in nginx','Nginx is a powerful web server and reverse proxy that can be used to implement load balancing for web applications. Load balancing allows incoming traffic to be distributed across multiple servers, improving performance, reliability, and scalability of your application.
+
+There are different ways to implement load balancing in Nginx, but one common approach is to use the upstream directive to define a group of backend servers, and then use the proxy_pass directive to forward client requests to these servers.
+
+Here there is an example configuration file for implementing load balancing in Nginx:
+
+markdown
+
+upstream backend {
+    server backend1.example.com;
+    server backend2.example.com;
+}
+
+server {
+    listen 80;
+    server_name example.com;
+
+    location / {
+        proxy_pass http://backend;
+    }
+}
+
+In this example, the upstream directive defines a group of backend servers, named backend, that will receive client requests. The server directives inside the upstream block specify the IP addresses or hostnames of the backend servers.
+
+The server block defines a virtual server that listens on port 80 and responds to requests for the example.com domain. The location block specifies that all requests to the server should be forwarded to the backend servers defined in the upstream block, using the proxy_pass directive.
+
+When a client sends a request to the Nginx server, Nginx will choose one of the backend servers in the backend group and forward the request to that server. By default, Nginx uses a simple round-robin algorithm to distribute requests across the backend servers, but this can be customized with additional load balancing options.
+
+Load balancing in Nginx can be configured to support different types of protocols, including HTTP, HTTPS, TCP, and UDP. It can also be used in conjunction with caching, SSL termination, and other advanced features to optimize the performance and security of your application.'),
+	 ('NGINX','NGINX5','SSL termination in NGINX','example of SSL termination in Nginx:
+
+
+server {
+    listen 80;
+    listen [::]:80;
+    server_name example.com;
+
+    return 301 https://$host$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name example.com;
+
+    ssl_certificate /path/to/cert.pem;
+    ssl_certificate_key /path/to/key.pem;
+
+    location / {
+        proxy_pass http://backend;
+    }
+}
+
+In this example, the first server block listens on port 80 and redirects all incoming requests to HTTPS using a return statement. This ensures that all traffic is encrypted before it reaches the server.
+
+The second server block listens on port 443 and specifies the SSL certificate and key to use for encryption. The ssl and http2 parameters enable SSL/TLS and HTTP/2 support, respectively.
+
+The location block specifies that all requests to the server should be forwarded to the backend servers, as in the previous example. The proxy_pass directive forwards the requests to the http://backend upstream group, which can contain one or more backend servers.
+
+By terminating SSL at the Nginx server, you can offload the resource-intensive task of decrypting SSL traffic from your backend servers, improving their performance and scalability. It also allows you to centralize SSL certificate management and implement advanced security features such as SSL client authentication, certificate revocation lists, and protocol filtering.');
